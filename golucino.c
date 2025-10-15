5e33e223247a046bfbcd6df44c839e74868a8a81
johnkennthgolucino-cmd <johnkennthgolucino@gmail.com>
2025-10-15 15:06:57 +0800

    Add files via upload
    Uploaded C case problem and output screenshot.

---
+++ golucino.c
@@ -0,0 +1,18 @@
+  #include <stdio.h>
+  
+  int main() {
+  
+      printf("Name: John kenneth C. Golucino\n");
+      printf("ID Number: 2025303885\n");
+      printf("------------------------\n");
+      
+      
+      int num1 = 45, num2 = 35, sum;
+      
+      sum = num1 + num2;
+      
+      printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+      
+      return 0;
+      
+   }  
